3e7e9401740e867f7cff4a8e2a784291c237e4bc
Rismanda09 <114470864+Rismanda09@users.noreply.github.com>
2023-03-17 22:49:46 +0700

    Add files via upload

---
+++ Pertemuan 3/Tugas/home.php
@@ -0,0 +1,12 @@
+<?php
+    include_once 'header.php'
+?> 
+		<h1>Welcome Home!</h1>
+		<p>
+			<!--Tuliskan Nama - NIM - Rombel -->
+			Rismanda Aulia Putri - 0110222102 - TI12
+		</p>
+<?php
+    include_once 'footer.php'
+?> 				
+					
